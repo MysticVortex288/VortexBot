731f60ccec1a4c24dcf48426ee16e697fc461619
MysticVortex288 <sukhjitmultani2012@gmail.com>
2025-03-21 16:38:53 +0100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import os
+import threading
+from main import bot
 
 app = Flask(__name__)
 
@@ -7,6 +9,15 @@ app = Flask(__name__)
 def home():
     return "Discord Bot is running!"
 
+def run_bot():
+    bot.run(os.getenv('DISCORD_TOKEN'))
+
+def start():
+    bot_thread = threading.Thread(target=run_bot)
+    bot_thread.start()
+    return bot_thread
+
 if __name__ == "__main__":
+    thread = start()
     port = int(os.getenv("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
